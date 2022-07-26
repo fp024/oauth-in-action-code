125c43f2e2f995569898f5f3b6caf39744d95f3a
fp024 <aopenasus@naver.com>
2022-07-27 07:00:15 +0900

    ch-4-ex-2 프로젝트 소스 정리

--- exercises/ch-4-ex-2/protectedResource.js
+++ exercises/ch-4-ex-2/protectedResource.js
@@ -1,4 +1,4 @@
-var express = require("express");
+var express = require('express');
 var bodyParser = require('body-parser');
 var cons = require('consolidate');
 var nosql = require('nosql').load('database.nosql');
@@ -18,76 +18,75 @@ app.use('/', express.static('files/protectedResource'));
 app.use(cors());
 
 var resource = {
-	"name": "Protected Resource",
-	"description": "This data has been protected by OAuth 2.0"
+  name: 'Protected Resource',
+  description: 'This data has been protected by OAuth 2.0',
 };
 
 var getAccessToken = function(req, res, next) {
-	var inToken = null;
-	var auth = req.headers['authorization'];
-	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
-		inToken = auth.slice('bearer '.length);
-	} else if (req.body && req.body.access_token) {
-		inToken = req.body.access_token;
-	} else if (req.query && req.query.access_token) {
-		inToken = req.query.access_token
-	}
-	
-	console.log('Incoming token: %s', inToken);
-	nosql.one().make(function(builder) {
-	  builder.where('access_token', inToken);
-	  builder.callback(function(err, token) {
-	    if (token) {
-	      console.log("We found a matching token: %s", inToken);
-	    } else {
-	      console.log('No matching token was found.');
-	    };
-	    req.access_token = token;
-	    next();
-	    return;
-	  });
-	});
+  var inToken = null;
+  var auth = req.headers['authorization'];
+  if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+    inToken = auth.slice('bearer '.length);
+  } else if (req.body && req.body.access_token) {
+    inToken = req.body.access_token;
+  } else if (req.query && req.query.access_token) {
+    inToken = req.query.access_token;
+  }
+
+  console.log('Incoming token: %s', inToken);
+  nosql.one().make(function(builder) {
+    builder.where('access_token', inToken);
+    builder.callback(function(err, token) {
+      if (token) {
+        console.log('We found a matching token: %s', inToken);
+      } else {
+        console.log('No matching token was found.');
+      }
+      req.access_token = token;
+      next();
+      return;
+    });
+  });
 };
 
 var requireAccessToken = function(req, res, next) {
-	if (req.access_token) {
-		next();
-	} else {
-		res.status(401).end();
-	}
+  if (req.access_token) {
+    next();
+  } else {
+    res.status(401).end();
+  }
 };
 
 var savedWords = [];
 
 app.get('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "read" scope
-	 */
-	res.json({words: savedWords.join(' '), timestamp: Date.now()});
+  /*
+   * Make this function require the "read" scope
+   */
+  res.json({ words: savedWords.join(' '), timestamp: Date.now() });
 });
 
 app.post('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "write" scope
-	 */
-	if (req.body.word) {
-		savedWords.push(req.body.word);
-	}
-	res.status(201).end();
+  /*
+   * Make this function require the "write" scope
+   */
+  if (req.body.word) {
+    savedWords.push(req.body.word);
+  }
+  res.status(201).end();
 });
 
 app.delete('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "delete" scope
-	 */
-	savedWords.pop();
-	res.status(204).end();
+  /*
+   * Make this function require the "delete" scope
+   */
+  savedWords.pop();
+  res.status(204).end();
 });
 
-var server = app.listen(9002, 'localhost', function () {
+var server = app.listen(9002, 'localhost', function() {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
 });
- 
