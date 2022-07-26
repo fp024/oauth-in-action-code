40332c0fc77fa2d318bf208ba05c015bfebb4b50
fp024 <aopenasus@naver.com>
2022-07-27 05:44:51 +0900

    4.2 액세스 토큰의 유효성 확인
    nosql 관련 사용이 책에 적힌 내용대로 하면 오류가 난다.
    저자님 깃허브 코드 참조해서 바꿨음.
    
    * https://www.npmjs.com/package/nosql

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -26,7 +26,7 @@ var resource = {
  * 유입된 요청에서 액세스 토큰 스캔
  * @param req   요청
  * @param res   응답
- * @param next  요청을 계속 계속해서 처리해기 위해 호출 되는 함수
+ * @param next  요청을 계속 계속해서 처리하기 위해 호출 되는 함수
  */
 var getAccessToken = function(req, res, next) {
   /*
@@ -47,18 +47,46 @@ var getAccessToken = function(req, res, next) {
 
   }
 
+  console.log('Incoming token: %s', inToken);
+
+  // nosql 사용법: https://www.npmjs.com/package/nosql
+  nosql.one().make(function(filter) {
+    filter.where('access_token', inToken);
+    filter.callback(function(err, token) {
+      if (token) {
+        console.log(`We found a matching token: ${inToken}`);
+      } else {
+        console.log('No matching token was found.');
+      }
+      req.access_token = token;
+      next();
+    });
+  });
 
 };
+
 
 app.options('/resource', cors());
 
 /*
  * Add the getAccessToken function to this handler
+ * 
+ * 핸들러로서 추가했음. getAccessToken() 가 핸들러로서 정상 처리되면
+ * 요청객체에 access_token 값이 토큰값이 정상 설정되거나 null로 설정됨
+ * 지면에는 cors()가 없는데... 저자님 예제 코드를 보니 cors 뒤에다 getAccessToken 을 배치하신 것 확인했다.
  */
-app.post('/resource', cors(), function(req, res) {
+app.post('/resource', cors(), getAccessToken, function(req, res) {
   /*
    * Check to see if the access token was found or not
+   *
+   * 토큰을 찾았는지 못찾았는지 여부 확인
+   * 못찾았다면 401응답
    */
+  if (req.access_token) {
+    res.json(resource);
+  } else {
+    res.status(401).end();
+  }
 });
 
 var server = app.listen(9002, 'localhost', function() {
